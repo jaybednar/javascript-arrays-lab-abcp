02b9afd0eb346b315f69099f1242211d0e2118fd
Jay Bednar <ironviralmedia@gmail.com>
2018-01-12 14:52:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 const app = "I don't do much."
 
+var kittens = ["Milo", "Otis", "Garfield"];
+
 function destructivelyAppendKitten(name) {
   kittens.push(name);
+}
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name);
 }
